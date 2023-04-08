688db65945ddf969c39e2115c414d897b203e4f5
enric-serra-sanz <enricserrasanz@gmail.com>
2023-04-08 16:20:56 +0100

    added validation to rules and age_band

--- src/fitfile/data_rules/abstract_data_rule.py
+++ src/fitfile/data_rules/abstract_data_rule.py
@@ -3,18 +3,26 @@ from abc import (
     abstractmethod,
 )
 import pandas
-from typing import Any
+from typing import (
+    Any,
+    List,
+    Optional,
+)
 
 
 class AbstractDataRule(ABC):
-    def __init__(self, dataframe: pandas.DataFrame, field_list: list[str] = None) -> None:
+    def __init__(self, dataframe: pandas.DataFrame, fields: Optional[List[str]] = None) -> None:
         self.dataframe: pandas.DataFrame = dataframe
-        self.field_list: list[str] = field_list
+        if fields is None:
+            fields = []
+        self.fields: List[str] = fields
 
     @abstractmethod
     def apply_rule(self) -> pandas.DataFrame:
         """
-        Apply this specific rule to the dataframe and return a transformed dataframe
+        Apply this specific rule to the dataframe and return a transformed dataframe, a no side
+        effect function, performance notice the deepcopy, if performance is an issue then modify
+        the original dataframe and live with side effects
         :return: A pandas.DataFrame with the specific transformation applied
         """
         pass
@@ -25,5 +33,15 @@ class AbstractDataRule(ABC):
         Transforms a single datapoint
         :param datum: A single datapoint
         :return: A transformed datapoint
+        """
+        pass
+
+    @abstractmethod
+    def on_validation_error(self, datum: Any, exception: Exception) -> None:
+        """
+        What to do when an entry fails to validate (usually log it, stop or take other actions)
+        :param datum: An entry
+        :param exception: The Exception to raise
+        :return: None
         """
         pass
--- src/fitfile/data_rules/age_band_rule.py
+++ src/fitfile/data_rules/age_band_rule.py
@@ -1,24 +1,72 @@
+import datetime
 import pandas
 
+from dateutil.relativedelta import relativedelta
+from typing import Union
 from fitfile.data_rules.abstract_data_rule import AbstractDataRule
+from copy import deepcopy
+
+
+class AgeDatumException(Exception):
+    pass
 
 
 class AgeBandRule(AbstractDataRule):
-    def __repr__(self):
+    def __repr__(self) -> str:
         return 'Rule one – Age Band Group'
 
     def apply_rule(self) -> pandas.DataFrame:
         """
-        Apply this specific rule to the dataframe and return a transformed dataframe, where all age, date of birth, and
-        year of birth fields are converted to a band of 10 up to 90+
+        Apply this specific rule to the dataframe and return a transformed dataframe, where all
+        age, date of birth, and year of birth fields are converted to a band of 10 up to 90+
         :return: A pandas.DataFrame with the specific transformation applied
         """
+        new_df = deepcopy(self.dataframe)
+        for field in self.fields:
+            new_df[field] = new_df[field].apply(self.transform_datum, True)
+        return new_df
 
-
-    def transform_datum(self, datum: Any) -> Any:
+    def transform_datum(self, datum: Union[str, int, float]) -> str:
         """
-        Transforms a single datapoint
+        Transforms a single datapoint, onto an age band of 10 up to 90+
         :param datum: A single datapoint
         :return: A transformed datapoint
+        """
+        try:
+            if isinstance(datum, int) or isinstance(datum, float):
+                if datum < 0:
+                    raise AgeDatumException(
+                        'Negative values are not supported for age, got {}'.format(
+                            datum
+                        ))
+                return self._to_ten_up_to_ninety_multiple(int(datum))
+            date_of_birth = datetime.date.fromisoformat(datum)
+            now = datetime.datetime.now()
+            return self._to_ten_up_to_ninety_multiple(relativedelta(now, date_of_birth).years)
+        except Exception as e:
+            self.on_validation_error(datum, e)
+            return str(datum)
+
+    @staticmethod
+    def _to_ten_up_to_ninety_multiple(age: int) -> str:
+        """
+        Converts an integer age to a string of the form '10 - 20' ... '90+'
+        :param age: An integer representing the age
+        :return: A string
+        """
+        modulo = min(90, (age // 10) * 10)
+        if modulo == 90:
+            return '90+'
+        return '{modulo} - {modulo_plus_ten}'.format(
+            modulo=modulo,
+            modulo_plus_ten=modulo + 10,
+        )
+
+    def on_validation_error(self, datum: Union[str, int, float], exception: Exception) -> None:
+        """
+        What to do when a validation fails
+        :param datum:
+        :param exception:
+        :return:
         """
         pass
